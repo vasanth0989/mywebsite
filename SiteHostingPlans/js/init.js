7c895ac770e0c67a9ab4a34009b1c211870c9fd5
Vasanth <vasanthsenthamaraikannan@gmail.com>
2015-09-30 10:41:19 +0530

    My Website
    Website

---
+++ SiteHostingPlans/js/init.js
@@ -0,0 +1,20 @@
+$(function(){
+	
+	$('.slider').mobilyslider({
+		content: '.sliderContent',
+		children: 'div',
+		transition: 'fade',
+		animationSpeed: 500,
+		autoplay: true,
+		autoplaySpeed: 3000,
+		pauseOnHover: false,
+		bullets: true,
+		arrows: true,
+		arrowsHide: true,
+		prev: 'prev',
+		next: 'next',
+		animationStart: function(){},
+		animationComplete: function(){}
+	});
+	
+});
